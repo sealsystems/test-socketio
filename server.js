31030debc4823307172353815af4433c2227c0c6
seal-mt <manfred.thein@sealsystems.de>
2018-03-22 11:29:06 +0100

    tlscert uses promises

--- server.js
+++ server.js
@@ -3,32 +3,34 @@
 /* eslint-disable no-console */
 const socketIO = require('socket.io');
 const tlscert = require('@sealsystems/tlscert');
-const server = require('https').createServer(tlscert.get());
+const https = require('https');
 
-const io = socketIO(server);
-const eventManagement = io.of('/event/management');
+tlscert.get().then((keystore) => {
+  const server = https.createServer(keystore).listen(3000);
 
-eventManagement.on('connection', (socket) => {
-  console.log('##### got new management connection');
+  const io = socketIO(server);
+  const eventManagement = io.of('/event/management');
 
-  socket.on('event', (event) => {
-    console.log('##### got management event', JSON.stringify(event, null, 2));
+  eventManagement.on('connection', (socket) => {
+    console.log('##### got new management connection');
+
+    socket.on('event', (event) => {
+      console.log('##### got management event', JSON.stringify(event, null, 2));
+    });
+  });
+
+  eventManagement.use((socket, next) => {
+    console.log('##### in middleware');
+    next();
+  });
+
+  const eventNotification = io.of('/event/notification');
+
+  eventNotification.on('connection', (socket) => {
+    console.log('##### got new notification connection');
+
+    socket.on('event', (event) => {
+      console.log('##### got notification event', JSON.stringify(event, null, 2));
+    });
   });
 });
-
-eventManagement.use((socket, next) => {
-  console.log('##### in middleware');
-  next();
-});
-
-const eventNotification = io.of('/event/notification');
-
-eventNotification.on('connection', (socket) => {
-  console.log('##### got new notification connection');
-
-  socket.on('event', (event) => {
-    console.log('##### got notification event', JSON.stringify(event, null, 2));
-  });
-});
-
-server.listen(3000);
